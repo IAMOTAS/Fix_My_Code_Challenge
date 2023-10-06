629c4a16d273d1458ed19fe2dcc9acca8b411dea
IAMOTAS <niiotasalokome@gmail.com>
2023-10-06 05:22:00 -0700

    Done

---
+++ 0x01-challenge/react-blog/src/components/JsxIncludes.js
@@ -0,0 +1,6 @@
+//This file was auto generated. Updating it will have no effect
+var JsxIncludes = {};
+JsxIncludes["/static/jsx/react-components-example.jsx"] = require("../../public/static/jsx/react-components-example.jsx");
+
+module.exports = JsxIncludes;
+
